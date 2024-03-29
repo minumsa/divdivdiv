e75e9a46bf3abaf0c1dfe2cc25b8857f983c7a13
유일 <83910706+minumsa@users.noreply.github.com>
2024-03-29 11:53:17 +0900

    Layout module CSS 생성

--- app/components/@common/Layout.tsx
+++ app/components/@common/Layout.tsx
@@ -1,7 +1,7 @@
 import { useAtomValue, useSetAtom } from "jotai";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
-import styles from "../../divdivdiv.module.css";
+import styles from "./Layout.module.css";
 import { languageAtom, showImageAtom } from "@/app/modules/atoms";
 import { Nav } from "../nav/Nav";
 
@@ -22,10 +22,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
           <div className={styles["nav-container"]}>
             <Nav />
           </div>
-          <div className={styles["content"]}>
-            {/* FIXME: 이미지 모달 사진 크기 때문에 window.innerWidth 사용해서 NoSSR 넣음. 추후에 미디어 쿼리 등 다른 방향으로 수정할 수 있으면 하고 NoSSR 제거하기.*/}
-            {children}
-          </div>
+          <div className={styles["content-container"]}>{children}</div>
         </div>
       </div>
     </div>
